92ce5379301c55e5ac0a9795367432bb8e5fc659
RoyRao2333 <royrao2333@gmail.com>
2022-04-11 16:23:15 +0800

    DateExtension

--- Swift/DateExtension.swift
+++ Swift/DateExtension.swift
@@ -19,4 +19,32 @@ extension Date {
     func component(_ component: Calendar.Component) -> Int {
         Calendar.current.component(component, from: self)
     }
+
+    func description(style: DateFormatter.Style = .short) -> String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = style
+        formatter.timeStyle = style
+
+        let daysBetween = daysBetween(date: Date())
+
+        if daysBetween == 0 {
+            return "Today".localized()
+        } else if daysBetween == 1 {
+            return "Yesterday".localized()
+        } else if daysBetween < 5 {
+            let weekdayIndex = Calendar.current.component(.weekday, from: self) - 1
+            return formatter.weekdaySymbols[weekdayIndex]
+        }
+
+        return formatter.string(from: self)
+    }
+
+    func daysBetween(date: Date) -> Int {
+        let calendar = Calendar.current
+        let startDate = calendar.startOfDay(for: self)
+        let endDate = calendar.startOfDay(for: date)
+        let components = calendar.dateComponents([.day], from: startDate, to: endDate)
+        
+        return components.day ?? 0
+    }
 }
