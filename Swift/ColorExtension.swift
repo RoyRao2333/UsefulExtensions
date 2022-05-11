20b9c5654c98eaffbb96b7736570bb42d6236b45
Roy Rao <royrao2333@gmail.com>
2022-05-12 00:12:29 +0800

    Update ColorExtension

--- Swift/ColorExtension.swift
+++ Swift/ColorExtension.swift
@@ -6,6 +6,18 @@
 
 import SwiftUI
 
+extension Color {
+    static let lightText = Color(UIColor.lightText)
+    static let darkText = Color(UIColor.darkText)
+    static let label = Color(UIColor.label)
+    static let secondaryLabel = Color(UIColor.secondaryLabel)
+    static let tertiaryLabel = Color(UIColor.tertiaryLabel)
+    static let quaternaryLabel = Color(UIColor.quaternaryLabel)
+    static let systemBackground = Color(UIColor.systemBackground)
+    static let secondarySystemBackground = Color(UIColor.secondarySystemBackground)
+    static let tertiarySystemBackground = Color(UIColor.tertiarySystemBackground)
+    static let contentBackground = Color("ContentBackgroundColor")
+}
 
 extension Color {
     
@@ -148,20 +160,6 @@ extension UIColor {
         )
         return includeAlpha ? hexString : String(hexString.dropLast(2))
     }
-}
-
-
-extension Color {
-    static let lightText = Color(UIColor.lightText)
-    static let darkText = Color(UIColor.darkText)
-    static let label = Color(UIColor.label)
-    static let secondaryLabel = Color(UIColor.secondaryLabel)
-    static let tertiaryLabel = Color(UIColor.tertiaryLabel)
-    static let quaternaryLabel = Color(UIColor.quaternaryLabel)
-    static let systemBackground = Color(UIColor.systemBackground)
-    static let secondarySystemBackground = Color(UIColor.secondarySystemBackground)
-    static let tertiarySystemBackground = Color(UIColor.tertiarySystemBackground)
-    static let contentBackground = Color("ContentBackgroundColor")
 }
 
 #endif
